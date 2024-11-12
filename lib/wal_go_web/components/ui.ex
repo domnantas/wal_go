7c8daea4d6ea1384b6c69e7818217448a8b26ff5
domnantas <hello@domnantas.lt>
2024-11-13 00:08:14 +0200

    Leftover changes

--- lib/wal_go_web/components/ui.ex
+++ lib/wal_go_web/components/ui.ex
@@ -4,7 +4,6 @@ defmodule WalGoWeb.UI do
 
   attr :class, :string, default: nil
   attr :rest, :global, include: ~w(type disabled form name value)
-
   slot :inner_block, required: true
 
   def button(assigns) do
@@ -20,6 +19,72 @@ defmodule WalGoWeb.UI do
     >
       <%= render_slot(@inner_block) %>
     </button>
+    """
+  end
+
+  attr :class, :string, default: nil
+  slot :inner_block, required: true
+
+  def card(assigns) do
+    ~H"""
+    <div class={merge(["rounded-lg border bg-card text-slate-900 shadow-sm", @class])}>
+      <%= render_slot(@inner_block) %>
+    </div>
+    """
+  end
+
+  attr :class, :string, default: nil
+  slot :inner_block, required: true
+
+  def card_header(assigns) do
+    ~H"""
+    <div class={merge(["flex flex-col gap-y-1.5 p-6", @class])}>
+      <%= render_slot(@inner_block) %>
+    </div>
+    """
+  end
+
+  attr :class, :string, default: nil
+  slot :inner_block, required: true
+
+  def card_title(assigns) do
+    ~H"""
+    <h3 class={merge(["text-2xl font-semibold leading-none tracking-tight", @class])}>
+      <%= render_slot(@inner_block) %>
+    </h3>
+    """
+  end
+
+  attr :class, :string, default: nil
+  slot :inner_block, required: true
+
+  def card_description(assigns) do
+    ~H"""
+    <p class={merge(["text-sm text-slate-500", @class])}>
+      <%= render_slot(@inner_block) %>
+    </p>
+    """
+  end
+
+  attr :class, :string, default: nil
+  slot :inner_block, required: true
+
+  def card_content(assigns) do
+    ~H"""
+    <div class={merge(["p-6 pt-0", @class])}>
+      <%= render_slot(@inner_block) %>
+    </div>
+    """
+  end
+
+  attr :class, :string, default: nil
+  slot :inner_block, required: true
+
+  def card_footer(assigns) do
+    ~H"""
+    <div class={merge(["flex items-center p-6 pt-0", @class])}>
+      <%= render_slot(@inner_block) %>
+    </div>
     """
   end
 end
--- lib/wal_go_web/live/user_login_live.ex
+++ lib/wal_go_web/live/user_login_live.ex
@@ -4,33 +4,31 @@ defmodule WalGoWeb.UserLoginLive do
   def render(assigns) do
     ~H"""
     <div class="mx-auto max-w-sm my-20">
-      <.header class="text-center">
-        Log in to account
-        <:subtitle>
-          Don't have an account?
-          <.link navigate={~p"/users/register"} class="font-semibold text-brand hover:underline">
-            Sign up
-          </.link>
-          for an account now.
-        </:subtitle>
-      </.header>
-
-      <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore">
-        <.input field={@form[:email]} type="email" label="Email" required />
-        <.input field={@form[:password]} type="password" label="Password" required />
-
-        <:actions>
-          <.input field={@form[:remember_me]} type="checkbox" label="Keep me logged in" />
-          <.link navigate={~p"/users/reset_password"} class="text-sm font-semibold">
-            Forgot your password?
-          </.link>
-        </:actions>
-        <:actions>
-          <.button phx-disable-with="Logging in..." class="w-full">
-            Log in <span aria-hidden="true">→</span>
-          </.button>
-        </:actions>
-      </.simple_form>
+      <.card>
+        <.card_header>
+          <.card_title>Log in</.card_title>
+          <.card_description>Enter your email below to log in to your account</.card_description>
+        </.card_header>
+        <.card_content>
+          <.form for={@form} id="login_form" action={~p"/users/log_in"} phx-change="validate">
+            <div class="space-y-8">
+              <.input field={@form[:email]} type="text" label="Email" />
+              <.input field={@form[:password]} type="password" label="Password" />
+              <div>
+                <.input field={@form[:remember_me]} type="checkbox" label="Keep me logged in" />
+                <.link navigate={~p"/users/reset_password"} class="text-sm font-semibold">
+                  Forgot your password?
+                </.link>
+              </div>
+              <div>
+                <.button phx-disable-with="Logging in..." class="w-full">
+                  Log in <span aria-hidden="true">→</span>
+                </.button>
+              </div>
+            </div>
+          </.form>
+        </.card_content>
+      </.card>
     </div>
     """
   end
